b09f2c2a464b9e74d67e29512cd03b6df52b8c3a
utsav <meenautsav1612@gmail.com>
2022-08-02 22:41:11 +0530

    Setting debug true temperarily to debug

--- talkish/settings.py
+++ talkish/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1:8000", "localhost", "https://talkish.herokuapp.com/"]
 
