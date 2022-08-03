2140cbb95d92c4cb453d153074e9f901df7710a2
utsav <meenautsav1612@gmail.com>
2022-08-03 11:33:39 +0530

    Debugging

--- talkish/settings.py
+++ talkish/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1:8000", "localhost", "https://talkish.herokuapp.com/"]
+ALLOWED_HOSTS = ["127.0.0.1:8000", "localhost", "talkish.herokuapp.com"]
 
 
 # Application definition
